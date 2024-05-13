72160d03de93dd6679b9039b5a6bde23c7f9a171
alok-i <alokmishra173@gmail.com>
2024-05-13 10:49:41 +0530

    socket connection done

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Welcome from "./Welcome.js";
 import Login from "./Auth/Login.js";
 import Register from "./Auth/Register.js";
 import Home from "./homePage.js";
+import { SocketProvider } from "./context/socket.js";
 
 function App() {
   return (
     <div>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <SocketProvider>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </SocketProvider>
     </div>
   );
 }
